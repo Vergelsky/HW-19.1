577e7dfbb24bebda50a3a17dadf180e35d699138
Dmitry Vergelsky <graf.werger@gmail.com>
2024-01-07 19:45:59 +0700

    Домашка 20.1.5 - таблица, фильтр, поиск сделан, исправлены ошибки

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
 
+from catalog.models import Product, Category
+
+
 # Register your models here.
+
+# admin.site.register(Product)
+# admin.site.register(Category)
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('id', 'product_name', 'price', 'category',)
+    list_filter = ('category',)
+    search_fields = ('product_name', 'product_description',)
+
+@admin.register(Category)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('id', 'category_name',)
--- catalog/models.py
+++ catalog/models.py
@@ -10,10 +10,16 @@ class Product(models.Model):
     category = models.ForeignKey('Category', on_delete=models.CASCADE, verbose_name='Категория')
     price = models.IntegerField(verbose_name='Цена')
     created_at = models.DateTimeField(verbose_name='Создано')
-    changed_at = models.CharField(verbose_name='Изменено')
+    changed_at = models.DateTimeField(verbose_name='Изменено', )
+
+    def __str__(self):
+        return self.product_name
 
 
 class Category(models.Model):
     category_name = models.CharField(max_length=120, verbose_name='Название')
     category_description = models.TextField(max_length=600, verbose_name='Описание')
-    created_at = models.DateTimeField(verbose_name='Создано')
+    created_at = models.DateTimeField(verbose_name='Создано', null=True)
+
+    def __str__(self):
+        return self.category_name
