eadbb15ea3eb84c65229d51561c2e5419f2c93f5
Dmitry Vergelsky <graf.werger@gmail.com>
2024-01-14 01:50:46 +0700

    Домашка 20.2.4 домашка готова.

--- catalog/urls.py
+++ catalog/urls.py
@@ -3,12 +3,15 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
+from catalog.apps import CatalogConfig
 from catalog.views import index, contacts, items
 
+app_name = CatalogConfig.name
+
 urlpatterns = [
-    path('', index),
-    path('contacts', contacts),
-    path('items', items)
+    path('', index, name='index'),
+    path('contacts', contacts, name='contacts'),
+    path('items', items, name='items'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 
--- catalog/views.py
+++ catalog/views.py
@@ -5,17 +5,29 @@ from catalog.models import Product
 
 # Create your views here.
 def index(request):
-    return render(request, 'catalog/index.html')
+    context = {
+        'some_text': "Какой-то текст для главной страницы",
+        'title': "Главная"
+    }
+
+    return render(request, 'catalog/index.html', context)
 
 
 def contacts(request):
-    return render(request, 'catalog/contacts.html')
+    context = {
+        'some_text': "Какой-то текст для страницы контактов",
+        'title': "Контакты"
+    }
+
+    return render(request, 'catalog/contacts.html', context)
 
 
 def items(request):
     items_list = Product.objects.all()
     context = {
-        'object_list': items_list
+        'object_list': items_list,
+        'some_text': "Какой-то текст для страницы товаров",
+        'title': "Товары"
     }
     print(type(context['object_list'][0]))
     return render(request, 'catalog/items.html', context)
