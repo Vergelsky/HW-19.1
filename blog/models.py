8162dfe46353988db9749ed99de10bb34445fb8e
Dmitry Vergelsky <graf.werger@gmail.com>
2024-01-17 10:18:19 +0700

    Домашка 21.1.2 - создал модель публикации и сделал миграцию

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class Blog(models.Model):
+    title = models.CharField(max_length=200, verbose_name='название')
+    content = models.TextField(verbose_name='содержимое')
+
+    class Meta:
+        verbose_name = 'публикация'
+        verbose_name_plural = 'публикации'
+
+    def __str__(self):
+        return f'Публикация "{self.title}"'
+
---
+++ catalog/management/commands/try_code.py
@@ -0,0 +1,22 @@
+from django.core.management import BaseCommand
+from django.utils.text import slugify
+
+
+
+class Command(BaseCommand):
+    def add_arguments(self, parser):
+        parser.add_argument('text', type=str, help="Текст для перевода в транслит")
+
+    def handle(self, *arg, **options):
+        rus_abc = ('абвгдеёжзийклмнопрстуфхцчшщьыъэюя')
+        slug_abc = ('a', 'b', 'v', 'g', 'd', 'e', 'jo', 'zh', 'z', 'i',
+                    'j', 'k', 'l', 'm', 'n', 'o', 'p', 'r', 's', 't', 'u', 'f',
+                    'h', 'c', 'ch', 'sh', 'sch', '', 'y', '', 'e', 'ju', 'ja')
+        trance_table = {}
+        for num in range(len(rus_abc)):
+            letter = rus_abc[num]
+            trance_table[ord(letter)] = slug_abc[num]
+
+        result = options['text'].lower().translate(trance_table)
+
+        return slugify(result))
